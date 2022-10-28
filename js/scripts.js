9937fdb855e62031bd676667a283ef8d3712ebcf
sulikey <anahi.sulikey@gmail.com>
2022-10-28 11:16:53 -0700

    WIP functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,11 @@
 window.addEventListener("load", function() {
 
   const form = document.getElementById("count");
-  form.addEventListener("submit", countDracula);
+  form.addEventListener("submit", numberToString);
 
 });
 
-//-------------------------------------------------------------
-   function countDracula(event) {
-  event.preventDefault();
-  
-  let testNumber = 1234
+let testNumber = 1234
 
 function numberToString(numberIn) {
   return numberIn.toString();
@@ -34,31 +30,3 @@ function arrayOfNumbers (arrayIn) {
   })
 return numberArray
 }
-
-  const num1 = parseInt(document.querySelector("input#input1").value);
-  const num2 = parseInt(document.querySelector("input#input2").value);
-
-if (num2 > num1) {
-document.getElementById("output").innerText = ("Please make 'count' larger than 'multiples of'")
-}
-
-else if (num1 === isNaN() || num2 === isNaN()) {
-document.getElementById("output").innerText = ("Please only use real numbers")
-}
-
-else if (num1 < 1 || num2 < 1) {
-document.getElementById("output").innerText = ("Please only use numbers of value '1' or more");
-}
-
-
-else {
-
-  let newArray = []
-  for (let index = 0; index <= num1; index += num2)
-  {newArray.push([index])
-  }
-  document.getElementById("output").innerText = (newArray)
-}
-   }
-
-   
